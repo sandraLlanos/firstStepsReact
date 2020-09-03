57619e58aff1bc5340cc9947e16bd195e7473b3d
Sandra Llanos <sllanos@bancolombia.com.co>
2020-09-03 16:14:46 -0500

    enzyme unit test

--- src/tests/PrimeraAppProps.test.js
+++ src/tests/PrimeraAppProps.test.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import PrimeraAppProps from '../PrimeraAppProps';
 
-describe('pruebas sobre <PrimeraAppProps >', () => {
-    test('debe de mostrar el mensaje "hola soy Sandra" ', () => {
+describe('pruebas sobre <PrimeraAppProps />', () => {
+    // test('debe de mostrar el mensaje "hola soy Sandra" ', () => {
         
+    //     const saludo = 'Hola soy Sandra';
+    //     // wrapper en este caso seria el prodicto renderizado de lo que se desea mostrar,
+    //     // es decir del compente de react.
+    //     // para esto es neceserio importar render de @testing-library/react
+    //     // render recibe el elemento que se  desea renderizar
+    //     // const wrapper = render( <PrimeraAppProps saludo={saludo}/>);
+    //     const { getByText } = render( <PrimeraAppProps saludo={saludo}/>);
+
+    //     // para esto se requiere la importacion de import '@testing-librar/jest-dom/extend-expect' de setupTest.js
+    //     // expect( getByText(saludo) ).toBeInTheDocument();
+    // });
+
+    test('debe de mostrar <PrimeraAppProps /> correctamete ', () => {
+
         const saludo = 'Hola soy Sandra';
-        // wrapper en este caso seria el prodicto renderizado de lo que se desea mostrar,
-        // es decir del compente de react.
-        // para esto es neceserio importar render de @testing-library/react
-        // render recibe el elemento que se  desea renderizar
-        // const wrapper = render( <PrimeraAppProps saludo={saludo}/>);
-        const { getByText } = render( <PrimeraAppProps saludo={saludo}/>);
+        const wrapper =  shallow( <PrimeraAppProps saludo={saludo}/> );
 
-        // para esto se requiere la importacion de import '@testing-librar/jest-dom/extend-expect' de setupTest.js
-        // expect( getByText(saludo) ).toBeInTheDocument();
-
-        
-
-
+        expect( wrapper ).toMatchSnapshot();
     })
+
+    
     
 })
